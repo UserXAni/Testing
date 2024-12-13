85e9eaf469f457eb3ae72aa14f63d492c944867e
UserXAni <dandiaj210900@gmail.com>
2024-12-14 05:35:40 +0700

    add deprecation message

--- src/com/example/application/Main.java
+++ src/com/example/application/Main.java
@@ -3,15 +3,17 @@ package com.example.application;
 // Current project: Tes
 
 public class Main {
+    private static boolean live;
+
+    protected static boolean isLive() {
+        return live;
+    }
 
 	public static void main(String[] args){
-	    
+        live = false;
+        for (int i = 0; i < 99999; i++) {
+            System.out.println("isLive: " + isLive());
+        }
 	}
-	
-	public static void y(String[] args){
-	    
-	}
-	
-	
 	
 }
